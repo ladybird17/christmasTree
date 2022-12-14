c9ee2c704c09b3dbb43c4d40554d3ace6775ee58
yjseo <yjseo@iochord.com>
2022-12-14 10:48:15 +0900

    feat: 기본 환경 구성

---
+++ src/main/java/com/demo/christmasTree/ChristmasTreeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.christmasTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasTreeApplication.class, args);
+	}
+
+}
